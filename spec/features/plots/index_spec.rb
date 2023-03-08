6a89c30f2211b641aa60335f24ecbdc5bcc935dc
Andra Helton <andrahelton@gmail.com>
2023-03-08 10:06:18 -0700

    Add corrections for plots index us1 test

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper' 
 
 RSpec.describe 'Plots Index Page', type: :feature do
-  let!(:plot1) { Plot.create!(number: 1, size: "Large", direction: "East") }
-  let!(:plot2) { Plot.create!(number: 2, size: "Small", direction: "North") }
-  let!(:plot3) { Plot.create!(number: 3, size: "Medium", direction: "West") }
+  let!(:garden1) { Garden.create!(name: "Turing Community Garden", organic: true) }
+
+  let!(:plot1) { Plot.create!(number: 1, size: "Large", direction: "East", garden_id: garden1.id) }
+  let!(:plot2) { Plot.create!(number: 2, size: "Small", direction: "North", garden_id: garden1.id) }
+  let!(:plot3) { Plot.create!(number: 3, size: "Medium", direction: "West", garden_id: garden1.id) }
 
   let!(:tomatos) { Plant.create!(name: "Tomatos", description: "It's tomatos", days_to_harvest: 100) }
   let!(:beans) { Plant.create!(name: "Beans", description: "It's Beans", days_to_harvest: 50) }
@@ -14,16 +16,16 @@ RSpec.describe 'Plots Index Page', type: :feature do
   let!(:cabbage) { Plant.create!(name: "Cabbage", description: "It's a cabbage", days_to_harvest: 150) }
 
   before do
-    PlotPlant.create!(plant: tomatos, plot: plot1)
-    PlotPlant.create!(plant: beans, plot: plot1)
+    PlantPlot.create!(plant: tomatos, plot: plot1)
+    PlantPlot.create!(plant: beans, plot: plot1)
 
-    PlotPlant.create!(plant: carrots, plot: plot2)
-    PlotPlant.create!(plant: cabbage, plot: plot2)
-    PlotPlant.create!(plant: tomatos, plot: plot2)
+    PlantPlot.create!(plant: carrots, plot: plot2)
+    PlantPlot.create!(plant: cabbage, plot: plot2)
+    PlantPlot.create!(plant: tomatos, plot: plot2)
 
-    PlotPlant.create!(plant: onions, plot: plot3)
-    PlotPlant.create!(plant: carrots, plot: plot3)
-    PlotPlant.create!(plant: beans, plot: plot3)
+    PlantPlot.create!(plant: onions, plot: plot3)
+    PlantPlot.create!(plant: carrots, plot: plot3)
+    PlantPlot.create!(plant: beans, plot: plot3)
 
     visit "/plots"
   end
