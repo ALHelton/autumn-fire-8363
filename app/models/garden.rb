abebe4d0c4755bfaba8028cd401232ae5c4773d7
Andra Helton <andrahelton@gmail.com>
2023-03-08 11:13:06 -0700

    Add passing model method

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,5 +2,7 @@ class Garden < ApplicationRecord
   has_many :plots
   has_many :plants, through: :plots
 
-  
+  def under_hundred_days_to_harvest_plants
+    plants.where('days_to_harvest < 100').distinct
+  end
 end
